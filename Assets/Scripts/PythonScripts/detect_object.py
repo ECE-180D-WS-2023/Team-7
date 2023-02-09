732c8f2d5a3e468312ed92f8aef5750e63111740
siwei-yuan <siwei_yuan@hotmail.com>
2023-02-08 16:28:37 -0800

    Change localization script to use MQTT

--- Assets/Scripts/PythonScripts/detect_object.py
+++ Assets/Scripts/PythonScripts/detect_object.py
@@ -1,7 +1,18 @@
 import cv2 as cv
 import time
 import signal
-import keyboard as kbd # importing keyboard module
+import paho.mqtt.client as mqtt
+import argparse
+
+
+def on_connect(client, userdata, flags, rc):
+  print("Connection returned result: " + str(rc))
+
+def on_disconnect(client, userdata, rc):
+  if rc != 0:
+    print('Unexpected Disconnect')
+  else:
+    print('Expected Disconnect')
 
 
 
@@ -19,27 +30,41 @@ def face_detect_func(img):
 
 if __name__ == '__main__':
 
-    curr_mode = 'Attack'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--topic", type=str, required=True)
+    args = parser.parse_args()
+    topic = args.topic
 
     vid = cv.VideoCapture(0)
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.connect_async('mqtt.eclipseprojects.io')
+    client.loop_start()
 
     def signal_handler(sig, frame):
         vid.release()
         cv.destroyAllWindows()
+        client.loop_stop()
+        client.disconnect()
 
     signal.signal(signal.SIGINT, signal_handler)
+
+    last_mode = 'Attack Mode'
 
     while True:
         ret, frame = vid.read()
         x_coord = face_detect_func(frame)
-        if int(x_coord) > 250:
-            new_mode = 'Defense'
+        if x_coord == -1:
+            new_mode = last_mode
+        elif int(x_coord) > 250:
+            new_mode = 'Defense Mode'
         else:
-            new_mode = 'Attack'
+            new_mode = 'Attack Mode'
     
-        if new_mode != curr_mode:
-            kbd.press_and_release('s')
-            curr_mode = new_mode
+        client.publish(topic, new_mode, qos=1)
 
+        last_mode = new_mode
+        
         time.sleep(1)
     
