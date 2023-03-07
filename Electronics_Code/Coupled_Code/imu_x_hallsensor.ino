d5aa5eaf3464ecc0a223b10d4eb5b3e48bd123fc
pjil127 <jillpantigcs@gmail.com>
2023-03-06 16:46:03 -0800

    Adding coupled code for IMU and Hall Sensor and Safekeeping of codes

--- Electronics_Code/Coupled_Code/imu_x_hallsensor.ino
+++ Electronics_Code/Coupled_Code/imu_x_hallsensor.ino
@@ -1,34 +1,7 @@
-/****************************************************************
- * Example7_DMP_Quat6_EulerAngles.ino
- * ICM 20948 Arduino Library Demo
- * Initialize the DMP based on the TDK InvenSense ICM20948_eMD_nucleo_1.0 example-icm20948
- * Paul Clark, April 25th, 2021
- * Based on original code by:
- * Owen Lyke @ SparkFun Electronics
- * Original Creation Date: April 17 2019
- * 
- * ** This example is based on InvenSense's _confidential_ Application Note "Programming Sequence for DMP Hardware Functions".
- * ** We are grateful to InvenSense for sharing this with us.
- * 
- * ** IMPORTANT NOTE: by default the DMP functionality is disabled in the library
- * ** as the DMP firmware takes up 14301 Bytes of program memory.
- * ** To use the DMP, you will need to:
- * ** Edit ICM_20948_C.h
- * ** Uncomment line 29: #define ICM_20948_USE_DMP
- * ** Save changes
- * ** If you are using Windows, you can find ICM_20948_C.h in:
- * ** Documents\Arduino\libraries\SparkFun_ICM-20948_ArduinoLibrary\src\util
- *
- * Please see License.md for the license information.
- *
- * Distributed as-is; no warranty is given.
- * 
- * Modified by Jillian Pantig for ECE 180D - Systems Design Capstone
- * The sensor print version update -Alex
- ***************************************************************/
+// Jillian Pantig
+// IMU Code is mostly taken from ICM 20948 Arduino Library Demo with modification
 
 #include "ICM_20948.h" // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
-#include <SparkFun_MicroPressure.h>
 
 #define SERIAL_PORT Serial
 
@@ -39,18 +12,24 @@
 #define AD0_VAL 1
 
 ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
-SparkFun_MicroPressure mpr; // Use default values with reset and EOC pins unused
-double pitch = 0.0;
+
+
+/************************  START HALL SENSOR VARIABLES ************************/
+
+#define Throttle_Sensor A0
+int throttle_extract = 0;
+
+/************************  END HALL SENSOR VARIABLES ************************/
 
 void setup()
 {
-  Serial.begin(115200);
-  Wire.begin();
+
   SERIAL_PORT.begin(115200); // Start the serial console
   SERIAL_PORT.println(F("ICM-20948 Example"));
 
-
   delay(100);
+
+  pinMode(Throttle_Sensor, INPUT);
 
   while (SERIAL_PORT.available()) // Make sure the serial RX buffer is empty
     SERIAL_PORT.read();
@@ -127,49 +106,18 @@ void setup()
     while (1)
       ; // Do nothing more
   }
-
-   if(!mpr.begin())
-  {
-    Serial.println("Cannot connect to MicroPressure sensor.");
-    while(1);
-  }
-
-
 }
-
-
-
 
 void loop()
 {
-
-  
-  // Read any DMP data waiting in the FIFO
-  // Note:
-  //    readDMPdataFromFIFO will return ICM_20948_Stat_FIFONoDataAvail if no data is available.
-  //    If data is available, readDMPdataFromFIFO will attempt to read _one_ frame of DMP data.
-  //    readDMPdataFromFIFO will return ICM_20948_Stat_FIFOIncompleteData if a frame was present but was incomplete
-  //    readDMPdataFromFIFO will return ICM_20948_Stat_Ok if a valid frame was read.
-  //    readDMPdataFromFIFO will return ICM_20948_Stat_FIFOMoreDataAvail if a valid frame was read _and_ the FIFO contains more (unread) data.
   icm_20948_DMP_data_t data;
   myICM.readDMPdataFromFIFO(&data);
 
   if ((myICM.status == ICM_20948_Stat_Ok) || (myICM.status == ICM_20948_Stat_FIFOMoreDataAvail)) // Was valid data available?
   {
-    //SERIAL_PORT.print(F("Received data! Header: 0x")); // Print the header in HEX so we can see what data is arriving in the FIFO
-    //if ( data.header < 0x1000) SERIAL_PORT.print( "0" ); // Pad the zeros
-    //if ( data.header < 0x100) SERIAL_PORT.print( "0" );
-    //if ( data.header < 0x10) SERIAL_PORT.print( "0" );
-    //SERIAL_PORT.println( data.header, HEX );
 
     if ((data.header & DMP_header_bitmap_Quat6) > 0) // We have asked for GRV data so we should receive Quat6
     {
-      // Q0 value is computed from this equation: Q0^2 + Q1^2 + Q2^2 + Q3^2 = 1.
-      // In case of drift, the sum will not add to 1, therefore, quaternion data need to be corrected with right bias values.
-      // The quaternion data is scaled by 2^30.
-
-      //SERIAL_PORT.printf("Quat6 data is: Q1:%ld Q2:%ld Q3:%ld\r\n", data.Quat6.Data.Q1, data.Quat6.Data.Q2, data.Quat6.Data.Q3);
-
       // Scale to +/- 1
       double q1 = ((double)data.Quat6.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
       double q2 = ((double)data.Quat6.Data.Q2) / 1073741824.0; // Convert to double. Divide by 2^30
@@ -186,24 +134,20 @@ void loop()
       double t2 = +2.0 * (q0 * q2 - q3 * q1);
       t2 = t2 > 1.0 ? 1.0 : t2;
       t2 = t2 < -1.0 ? -1.0 : t2;
-      pitch = asin(t2) * 180.0 / PI;
+      double pitch = asin(t2) * 180.0 / PI;
 
+      SERIAL_PORT.print(F(" Steering Angle:"));
+      SERIAL_PORT.println(pitch, 1);
     }
-  if (! isnan(pitch)) {
-            SERIAL_PORT.print(F(" Steering Angle:"));
-            SERIAL_PORT.println(pitch, 1);
-            Serial.print(mpr.readPressure(PA),1);
-            Serial.println(" Pa");
-            Serial.print(mpr.readPressure(KPA),4);
-            Serial.println(" kPa");
-      }
-      
-       delay(1000);    
-      
   }
 
   if (myICM.status != ICM_20948_Stat_FIFOMoreDataAvail) // If more data is available then we should read it right away - and not delay
   {
     delay(10);
   }
+
+  throttle_extract = analogRead(Throttle_Sensor); // read the analog Throttle input
+  int throttle = map(throttle_extract, 2690, 8191, 0, 100);
+  float throttle_float = (float)throttle/100;
+  Serial.println(throttle_float);
 }
