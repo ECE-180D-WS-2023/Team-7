6fc9bc06710471df23b2c23551f604be1e12757b
Jerry Yuan <siwei_yuan@hotmail.com>
2023-03-16 14:14:08 -0700

    Optimize MQTT initialization process

--- Assets/Scripts/InitializeMQTT.cs
+++ Assets/Scripts/InitializeMQTT.cs
@@ -19,7 +19,14 @@ public class InitializeMQTT : MonoBehaviour
             Debug.Log("MQTT initializing...");
             GetComponent<MQTTController>().playerToControl = player;
             GetComponent<MQTTController>()._eventSender.topicSubscribe = "ECE180D/team7/" + player.GetComponent<NetworkInfo>().PlayerID.ToString();
-            GetComponent<MQTTController>()._eventSender.Connect();
+            //GetComponent<MQTTController>()._eventSender.Connect();
         }
     }
+
+    private IEnumerator AsyncConnect()
+    {
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<MQTTController>()._eventSender.Connect();
+    }
+
 }
